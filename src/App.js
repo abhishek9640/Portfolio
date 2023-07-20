3494570991dd2fc577498b79c4ae6b3479c33c37
abhishek9640 <abhishekgupta9640@gmail.com>
2023-07-21 02:11:56 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SocialLink from "./components/SocialLink";
+
+function App() {
+  return (
+    <div>
+     <NavBar />
+     <Home />
+     <SocialLink />
+    </div>
+  );
+}
+
+export default App;
